445c3868689d75de7b87f2cc7830fccfa4431371
Nova <technobaboo@gmail.com>
2023-10-16 10:08:52 -0400

    fix: clean to fix https://github.com/xamarin/xamarin-android/issues/7876

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+dotnet clean Projects/Android/stereokit_lynx_template_Android.csproj
 dotnet publish -c Release Projects/Android/stereokit_lynx_template_Android.csproj
 adb install Projects/Android/bin/Release/net7.0-android/technobaboo.stereokit_lynx_template-Signed.apk
